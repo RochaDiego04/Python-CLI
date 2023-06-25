951d2d456dff70b1e220cfda9e7ba0f69f1f9ae4
Diego Rocha <drochaesc@gmail.com>
2023-06-24 23:46:44 -0600

    Function to add new users into the .json

--- cli.py
+++ cli.py
@@ -6,6 +6,26 @@ def cli():
     pass
 
 @cli.command()
+@click.option('--name', required=True, help="Name of the user")
+@click.option('--lastname', required=True, help="Name of the user")
+@click.pass_context
+def new(ctx, name, lastname):
+    if not name or not lastname:
+        ctx.fail('The name and lastname are required')
+    else:
+        data = json_manager.read_json()
+        new_id = len(data) + 1
+        new_user = {
+            'id': new_id,
+            'name': name,
+            'lastname': lastname
+        }
+        data.append(new_user)
+        json_manager.write_json(data)
+        print(f"User {name} {lastname} created successfully with id {new_id}")
+
+
+@cli.command()
 def user():
     users = json_manager.read_json()
     for user in users:
--- json_manager.py
+++ json_manager.py
@@ -14,5 +14,6 @@ def read_json():
         data = json.load(f)
     return data
 
-def write_json():
-    pass
+def write_json(data):
+    with open('data.json', 'w') as f:
+        json.dump(data, f)
